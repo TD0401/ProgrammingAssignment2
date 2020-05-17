c2cb2c513039e6f1458060b1b36d3dddd6b2c80e
Trina Dey <trina.dumca@gmail.com>
2020-05-17 21:24:17 +0530

    Adding the code for calculating and caching inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This programs solves the inverse of a matrix with an assumption that
+## the inverse of the matrix exists and then caches it. 
 
-## Write a short comment describing this function
+## This function stores the matrix and the inverse of the matrix 
+## outside the current environment.This function returns a list containing functions to get or set the matrix from or to
+## outside the current environment and to get or set the inverse of the matrix from or to outside the current environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat<- NULL
+    
+    # sets the matrix
+    setMat<- function(my_mat){
+        x <<- my_mat
+        inv_mat <<- NULL
+    }
+    
+    # gets the matrix 
+    getMat <- function() {
+        x
+    }
+    
+    # Sets the inverse of the matrix
+    setInvMat <- function(inverse) {
+        inv_mat <<- inverse
+    }
+    
+    # Gets the inverse of the matrix
+    getInvMat <- function() {
+        inv_mat
+    }
+    
+    # returns the list of functions
+    list(setMat = setMat, getMat = getMat,
+         setInvMat = setInvMat,
+         getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated (and the matrix has not
+## changed), then the "cachesolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # Fetches the inverse of matrix from makeCacheMatrix functions
+    inv_mat <- x$getInvMat()
+    
+    # if inverse exists return as is
+    if(!is.null(inv_mat)) {
+        message("getting cached data for inverse of matrix")
+        return(inv_mat)
+    }
+    
+    # if inverse doesnt exist, fetch the matrix, calcualte and set it back 
+    my_mat <- x$getMat()
+    inv_mat <- solve(my_mat, ...) 
+    x$setInvMat(inv_mat)
+    
+    #return the inverse of matrix calculated
+    inv_mat
 }
